b43a954904339e6499462221a35e38770fc4b10b
christofyzhao <146423350+christofyzhao@users.noreply.github.com>
2025-03-05 13:53:14 +0800

    Update A股_full data_interaction_med.R
    roa to roe

--- A股_full data_interaction_med.R
+++ A股_full data_interaction_med.R
@@ -17,14 +17,14 @@ data_long <- data_raw %>%
   pivot_longer(cols = -c(code, name, field, nature, fcode, regis_place), 
                names_to = c(".value", "year"),
                names_pattern = "([a-zA-Z_]+)(\\d{4})") %>% mutate(
-    ln_size = log((size / 1e8) + 1), 
-    ln_mc = log((mc / 1e8) + 1),
-    ESG = case_when(
-      ESG == "AAA" ~ 9, ESG == "AA" ~ 8, ESG == "A" ~ 7,
-      ESG == "BBB" ~ 6, ESG == "BB" ~ 5, ESG == "B" ~ 4,
-      ESG == "CCC" ~ 3, ESG == "CC" ~ 2, ESG == "C" ~ 1,
-      TRUE ~ NA_real_
-    )) %>% mutate(ESG = as.numeric(ESG), year = as.factor(year))
+                 ln_size = log((size / 1e8) + 1), 
+                 ln_mc = log((mc / 1e8) + 1),
+                 ESG = case_when(
+                   ESG == "AAA" ~ 9, ESG == "AA" ~ 8, ESG == "A" ~ 7,
+                   ESG == "BBB" ~ 6, ESG == "BB" ~ 5, ESG == "B" ~ 4,
+                   ESG == "CCC" ~ 3, ESG == "CC" ~ 2, ESG == "C" ~ 1,
+                   TRUE ~ NA_real_
+                 )) %>% mutate(ESG = as.numeric(ESG), year = as.factor(year))
 
 
 #clean
@@ -44,10 +44,10 @@ data_long_clean <- data_long %>% filter(!is.na(ESG) & !is.na(ln_size) & !is.na(R
                                         & !is.na(leverage) & !is.na(tobinq)) # 关键变量缺失的行删掉
 
 
-# # 用中位数填充
+# 用中位数填充
 # fill_median <- function(x) ifelse(is.na(x), median(x, na.rm = TRUE), x)
 # 
-# data_long_clean <- data_long_clean %>% mutate(across(c(asset_turnover, ins_hold, CR, ROE), fill_median))
+# data_long_clean <- data_long_clean %>% mutate(across(c(asset_turnover, ins_hold, CR), fill_median))
 # 
 # colSums(is.na(data_long))
 
@@ -73,7 +73,7 @@ data_long_clean <- data_long_clean %>% mutate(across(winsor_vars, winsorization)
 # 检查清洗后样本量
 nrow(data_long_clean)
 # 相关性矩阵
-correlation_matrix <- cor(data_long_clean[, c("ESG", "ln_mc", "ln_size", "ROA", 
+correlation_matrix <- cor(data_long_clean[, c("ESG", "ln_mc", "ln_size", "ROE", 
                                               "leverage", "tobinq", "growth", "CR", 
                                               "ins_hold", "asset_turnover", "IR")], use = "pairwise.complete.obs")
 print(correlation_matrix)
@@ -111,20 +111,20 @@ data_long_clean <- data_long_clean %>%
 
 #国企/私企/外资
 #basic
-model_interaction_soe <- lm(ln_mc ~ ESG * nature_soe + ln_size + ROA + leverage + 
-                              tobinq + growth + CR + factor(year) + field_id, data = data_long_clean)
+model_interaction_soe <- lm(ln_mc ~ ESG * nature_soe + ln_size + ROE + leverage + 
+                              tobinq + CR + factor(year) + field_id, data = data_long_clean)
 summary(model_interaction_soe)
 
 # 固定效应国企/私企/外资
-model_fe <- plm(ln_mc ~ ESG * nature_soe + ln_size + ROA + leverage + 
-                  tobinq + growth + CR + year, 
+model_fe <- plm(ln_mc ~ ESG * nature_soe + ln_size + ROE + leverage + 
+                  tobinq + CR + year, 
                 data = data_long_clean, index = c("code", "year"), model = "within")
 summary(model_fe)
 
 # 随机效应国企/私企/外资
 
-model_re <- plm(ln_mc ~ ESG * nature_soe + ln_size + ROA + leverage + 
-                  tobinq + growth + CR + factor(year) + field_id, 
+model_re <- plm(ln_mc ~ ESG * nature_soe + ln_size + ROE + leverage + 
+                  tobinq + CR + factor(year) + field_id, 
                 data = data_long_clean, index = c("code", "year"), model = "random")
 summary(model_re)
 
@@ -135,21 +135,21 @@ phtest(model_fe, model_re)
 #重污染/污染
 #basic
 model_interaction_pollute <- lm(ln_mc ~ ESG * field_id + ln_size + 
-                                  ROA + leverage + tobinq + growth + nature_soe + factor(year), 
+                                  ROE + leverage + tobinq + nature_soe + factor(year), 
                                 data = data_long_clean)
 summary(model_interaction_pollute)
 
 # 固定效应污染/重污染
-model_fe2 <- plm(ln_mc ~ ESG * field_id + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + factor(year), 
+model_fe2 <- plm(ln_mc ~ ESG * field_id + ln_size + ROE + leverage + 
+                   tobinq + CR + factor(year), 
                  data = data_long_clean, index = c("code", "year"), model = "within")
 summary(model_fe2)
 
 
 
 # 随机效应污染/重污染
-model_re2 <- plm(ln_mc ~ ESG * field_id + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + factor(year), 
+model_re2 <- plm(ln_mc ~ ESG * field_id + ln_size + ROE + leverage + 
+                   tobinq + CR + factor(year), 
                  data = data_long_clean, index = c("code", "year"), model = "random")
 summary(model_re2)
 
@@ -158,20 +158,20 @@ phtest(model_fe2, model_re2)
 
 
 # OLS 基础回归
-model_basic <- lm(ln_mc ~ ESG + ln_size + ROA + leverage + 
-                    tobinq + growth + CR + nature_soe + field_id + factor(year), 
+model_basic <- lm(ln_mc ~ ESG + ln_size + ROE + leverage + 
+                    tobinq + CR + nature_soe + field_id + factor(year), 
                   data = data_long_clean)
 summary(model_basic) 
 
 # 固定效应
-model_fe3 <- plm(ln_mc ~ ESG + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + nature_soe + field_id + factor(year), data = data_long_clean, 
+model_fe3 <- plm(ln_mc ~ ESG + ln_size + ROE + leverage + 
+                   tobinq + CR + nature_soe + field_id + factor(year), data = data_long_clean, 
                  index = c("code", "year"), model = "within")
 summary(model_fe3)
 
 # 随机效应
-model_re3 <- plm(ln_mc ~ ESG + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + nature_soe + field_id + factor(year), data = data_long_clean, 
+model_re3 <- plm(ln_mc ~ ESG + ln_size + ROE + leverage + 
+                   tobinq + CR + nature_soe + field_id + factor(year), data = data_long_clean, 
                  index = c("code", "year"), model = "random")
 summary(model_re3)
 
@@ -187,30 +187,30 @@ phtest(model_fe3, model_re3)
 # 中介效应分析
 
 # esg to 资产周转率
-model_esg_at <- lm(formula = asset_turnover ~ ESG + ln_size + ROA + leverage + tobinq + 
-                     growth + CR + field_id + year + nature_soe, data = data_long_clean)
+model_esg_at <- lm(formula = asset_turnover ~ ESG + ln_size + ROE + leverage + tobinq + 
+                     CR + field_id + year + nature_soe, data = data_long_clean)
 
 
 # esg to 机构持股
-model_esg_ih <-lm(ins_hold ~ ESG + ln_size + ROA + leverage + tobinq + growth + CR + 
+model_esg_ih <-lm(ins_hold ~ ESG + ln_size + ROE + leverage + tobinq + CR + 
                     field_id + year + nature_soe, data = data_long_clean)
 
 # esg to IR
-model_esg_ir <-lm(IR ~ ESG + ln_size + ROA + leverage + tobinq + growth + CR + 
+model_esg_ir <-lm(IR ~ ESG + ln_size + ROE + leverage + tobinq + CR + 
                     field_id + year + nature_soe, data = data_long_clean)
 
 # 资产周转率 to mc
-model_med1 <- lm( ln_mc ~ ESG + asset_turnover + ln_size + ROA + leverage + 
-                    tobinq + growth + CR + field_id + year + nature_soe, data = data_long_clean)
+model_med1 <- lm( ln_mc ~ ESG + asset_turnover + ln_size + ROE + leverage + 
+                    tobinq + CR + field_id + year + nature_soe, data = data_long_clean)
 
 
 # 机构持股 to mc
-model_med2 <- lm(ln_mc ~ ESG + ins_hold + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + field_id + year + nature_soe, data = data_long_clean)
+model_med2 <- lm(ln_mc ~ ESG + ins_hold + ln_size + ROE + leverage + 
+                   tobinq + CR + field_id + year + nature_soe, data = data_long_clean)
 
 #IR to mc
-model_med3 <- lm(ln_mc ~ ESG + IR + ln_size + ROA + leverage + 
-                   tobinq + growth + CR + field_id + year + nature_soe, data = data_long_clean)
+model_med3 <- lm(ln_mc ~ ESG + IR + ln_size + ROE + leverage + 
+                   tobinq + CR + field_id + year + nature_soe, data = data_long_clean)
 
 # # 确保中介变量是数值型
 # data_long_clean <- data_long_clean %>%
